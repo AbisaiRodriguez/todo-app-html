e41a54b979160546ad98b0aa74be824551ccf9f6
Abisai Rodriguez <abisai.rodriguez13@gmail.com>
2019-02-10 21:35:05 -0800

    Fixed server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require('path')
 //create the express app
 const app = express()
 //create middleware t0 handle the serving the app
-app.use("/", serveStatic ( path.join(_dirname, '/public')))
+app.use("/", serveStatic ( path.join(__dirname, '/public')))
 //serve index by default
-app.get('/', function (req, res) {res.sendFile(_dirname + '/public/index.html')})
+app.get('/', function (req, res) {res.sendFile(__dirname + '/public/index.html')})
 //creat default port to serve the app on
 const port = process.env.PORT || 5000
 app.listen(port)
